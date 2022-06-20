967a978175ae125eac8a23b81727929ae12c40dd
Luan Victor Leite da Silva <luan.silva@me.com.br>
2022-06-20 16:24:33 -0300

    Aula 12 - Questão 1

---
+++ Aula-12/Matriz.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Aula_12
+{
+    internal class Matriz
+    {
+        public char[,] num = new char[,] 
+        {  
+           { '7', '8', '9' },
+           { '4', '5', '6' },
+           { '1', '2', '3' },
+        };
+
+        public void imprime()
+        {
+            for (int i=0; i<num.GetLength(0); i++)
+            {
+                for (int j = 0; j < num.GetLength(1); j++)
+                {   
+                    if (j == 1 || j == 2)
+                    {
+                        Console.Write(" | ");
+                    }
+                    
+                    Console.Write($" {num[i, j]}");
+
+                    
+                }
+                if (i == 0 || i == 1)
+                {
+                    Console.WriteLine("\n------------");
+                }
+                
+            }
+        }
+    }
+}
+
---
+++ Aula-12/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matriz matriz = new Matriz();
+
+            matriz.imprime();
+        }
+
+    }
+}
